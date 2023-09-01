6fe2e32387843911fa795cab1e1a6e17450fa406
VividReality <thevividreality@gmail.com>
2023-09-02 00:19:36 +0200

    collission

--- UselessMage/Assets/Maps/Tiles/thicket tiles.tsx
+++ UselessMage/Assets/Maps/Tiles/thicket tiles.tsx
@@ -1,8 +1,52 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="thicket tiles" tilewidth="10" tileheight="14" tilecount="16" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="thicket tiles" tilewidth="10" tileheight="14" tilecount="29" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="36">
+  <image width="10" height="14" source="NO.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0835738" y="-0.0557158" width="10.1124" height="10.1403"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <image width="10" height="14" source="water.png"/>
+ </tile>
  <tile id="0">
   <image width="10" height="14" source="dirt.png"/>
+ </tile>
+ <tile id="14">
+  <image width="10" height="14" source="dirt1.png"/>
+ </tile>
+ <tile id="18">
+  <image width="10" height="14" source="dirt corner NW.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.195005" y="10.196">
+    <polygon points="0.0278579,-0.0557158 3.23152,-6.1566 10.2517,-10.2796 -0.0278579,-10.3353"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <image width="10" height="14" source="dirt corner NE.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.1124" y="10.0846">
+    <polygon points="0,0 -1.86648,-7.52164 -10.2796,-10.2239 0.0278579,-10.2239"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <image width="10" height="14" source="dirt corner SW.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.250721" y="4.2344">
+    <polygon points="0,0 3.31509,4.59656 10.391,5.90588 10.391,9.75027 0.0278579,9.91742"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <image width="10" height="14" source="dirt corner SE.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.0289" y="2.86937">
+    <polygon points="0.0835738,-0.0557158 -2.06149,4.93085 -10.1681,7.2152 -10.1681,11.3103 0.0557158,11.171"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="1">
   <image width="10" height="14" source="grass.png"/>
@@ -13,40 +57,84 @@
  <tile id="8">
   <image width="10" height="14" source="grass border L.png"/>
  </tile>
- <tile id="9">
-  <image width="10" height="14" source="grass border R.png"/>
- </tile>
  <tile id="10">
   <image width="10" height="14" source="grass border U.png"/>
  </tile>
- <tile id="11">
-  <image width="10" height="14" source="flowers.png"/>
+ <tile id="9">
+  <image width="10" height="14" source="grass border R.png"/>
  </tile>
  <tile id="12">
   <image width="10" height="14" source="grassblade.png"/>
  </tile>
+ <tile id="11">
+  <image width="10" height="14" source="flowers.png"/>
+ </tile>
  <tile id="13">
   <image width="10" height="14" source="flowers1.png"/>
  </tile>
- <tile id="14">
-  <image width="10" height="14" source="dirt1.png"/>
- </tile>
  <tile id="15">
   <image width="10" height="14" source="rock.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.780022" y="4.34584" width="8.38524" height="3.87225"/>
+  </objectgroup>
  </tile>
- <tile id="16">
-  <image width="10" height="14" source="NO.png"/>
+ <tile id="25">
+  <image width="10" height="14" source="ice.png"/>
  </tile>
- <tile id="17">
-  <image width="10" height="14" source="dirt corner NE.png"/>
+ <tile id="26">
+  <image width="10" height="14" source="ice1.png"/>
  </tile>
- <tile id="18">
-  <image width="10" height="14" source="dirt corner NW.png"/>
+ <tile id="21">
+  <image width="10" height="14" source="ice corner NE.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0835738" y="-0.13929">
+    <polygon points="0,0 10.2796,10.1681 10.1681,-0.13929"/>
+   </object>
+  </objectgroup>
  </tile>
- <tile id="19">
-  <image width="10" height="14" source="dirt corner SE.png"/>
+ <tile id="22">
+  <image width="10" height="14" source="ice corner NW.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.1124" y="-0.167148">
+    <polygon points="0,0 -10.3631,10.2239 -10.2517,-0.195005"/>
+   </object>
+  </objectgroup>
  </tile>
- <tile id="20">
-  <image width="10" height="14" source="dirt corner SW.png"/>
+ <tile id="23">
+  <image width="10" height="14" source="ice corner SE.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.1124" y="-0.0278579">
+    <polygon points="0,0 -10.2796,10.3074 -10.2517,14.2633 0.0278579,14.2911"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <image width="10" height="14" source="ice corner SW.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.167148" y="-0.0835738">
+    <polygon points="0,0 10.3074,10.2239 10.3353,14.1518 0.0278579,14.1797"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <image width="10" height="14" source="snow.png"/>
+ </tile>
+ <tile id="28">
+  <image width="10" height="14" source="snow border D.png"/>
+ </tile>
+ <tile id="29">
+  <image width="10" height="14" source="snow border L.png"/>
+ </tile>
+ <tile id="31">
+  <image width="10" height="14" source="snow border U.png"/>
+ </tile>
+ <tile id="30">
+  <image width="10" height="14" source="snow border R.png"/>
+ </tile>
+ <tile id="32">
+  <image width="10" height="14" source="icicle.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.835738" y="6.1566" width="8.32952" height="4.0394"/>
+  </objectgroup>
  </tile>
 </tileset>
