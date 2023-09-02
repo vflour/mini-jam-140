83683d203af255d8f74e3ff1188dfd95ba23cf64
vflour <48340411+vflour@users.noreply.github.com>
2023-09-02 13:21:06 -0400

    Add enemy anims and change folders for enem generation

--- UselessMage/Assets/Scripts/Enemy/Enemy.cs
+++ UselessMage/Assets/Scripts/Enemy/Enemy.cs
@@ -11,6 +11,7 @@ public class Enemy : MonoBehaviour
     public GameObject target;
     public float moveSpeed;
     private Rigidbody2D rb;
+    private Collider2D collider;
 
     public int attackDamage;
     public float damageRange;
@@ -18,30 +19,49 @@ public class Enemy : MonoBehaviour
     void Start(){
         rb = this.GetComponent<Rigidbody2D>();
         enemyAnimator = GetComponent<Animator>();
+        collider = GetComponent<Collider2D>();
     }
 
     void FixedUpdate(){
-        Vector3 targetDir = target.transform.position - transform.position;
+        Vector3 targetDir = EnemyAnnoyanceState != AnnoyanceState.Enraged ? target.transform.position - transform.position : OffscreenTarget - transform.position;
         rb.velocity = targetDir * moveSpeed;
         var opposite = -rb.velocity;
+       
+        if (EnemyAnnoyanceState != AnnoyanceState.Enraged)
+        {
+            if(Stun>0){
+                //rb.velocity = Vector2.zero;
+                Stun--;
+            }
+
+            if(KnockBack>0){
+                rb.AddRelativeForce(Vector3.Normalize(opposite) * KnockBack * 0.1f, ForceMode2D.Impulse);
+                KnockBack = KnockBack/2;
+            }
         
-        if(Stun>0){
-            rb.velocity = Vector2.zero;
-            Stun--;
+            var nearTarget = Vector2.Distance(this.transform.position,target.transform.position);
+            if (nearTarget < damageRange){
+                target.GetComponent<PlayerLogic>().health -= attackDamage;
+            }
         }
-
-        if(KnockBack>0){
-            rb.velocity = opposite * KnockBack;
-            KnockBack = 0;
-        }
-
-        var nearTarget = Vector2.Distance(this.transform.position,target.transform.position);
-        if (nearTarget < damageRange){
-            target.GetComponent<PlayerLogic>().health -= attackDamage;
-        }
-
-        if(Annoyance>=maxAnnoyance){
+        else if(targetDir.magnitude < 0.01) {
             Destroy(this.gameObject);
+        }
+    }
+
+    // cached value
+    private Vector3 _offscreenTarget = Vector3.zero;
+    private Vector3 OffscreenTarget { 
+        get 
+        {
+            if (_offscreenTarget == Vector3.zero)
+            {
+                var camera = Camera.current;
+                var position = camera.WorldToScreenPoint(transform.position);
+                var extremity = position.x >= camera.pixelWidth/2 ? camera.pixelWidth + 100 : -100;
+                _offscreenTarget = camera.ScreenToWorldPoint(new Vector3(extremity, position.y));
+            }
+            return _offscreenTarget;
         }
     }
     
@@ -52,7 +72,9 @@ public class Enemy : MonoBehaviour
         private set
         {
             _annoyanceState = value;
-            enemyAnimator.Play(_annoyanceState.ToString(), -1, 0);
+            enemyAnimator.Play(_annoyanceState.ToString(), 0, 0);
+            if (_annoyanceState == AnnoyanceState.Enraged)
+                collider.enabled = false;
         }
         get
         {
@@ -66,17 +88,18 @@ public class Enemy : MonoBehaviour
     {
         set 
         {
-            bool hit = _annoyance > value;
+            bool hit = _annoyance <= value;
+            Debug.Log(_annoyance);
             _annoyance = Mathf.Clamp(value, 0, maxAnnoyance);
             
             if (!hit) return;
-            
+             
             bool firstHit = EnemyAnnoyanceState == AnnoyanceState.Idle; 
             if (firstHit)
                 EnemyAnnoyanceState = AnnoyanceState.Surprised;
-            else if (_annoyance == 0)
+            else if (_annoyance >= maxAnnoyance)
                 EnemyAnnoyanceState = AnnoyanceState.Enraged;
-            else if (0.5f <= _annoyance/maxAnnoyance )
+            else if (0.5f <= _annoyance/(float)maxAnnoyance )
                 EnemyAnnoyanceState = AnnoyanceState.Annoyed;
                 
         }
